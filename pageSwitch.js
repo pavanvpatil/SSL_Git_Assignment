a6410f2a2f7ea2228771da333f809e3c22d4c2b7
pavanvpatil <pavanvpatil01@gmail.com>
2021-10-26 22:14:15 +0530

    first commit

---
+++ pageSwitch.js
@@ -0,0 +1,12 @@
+function page1(){
+    location.replace("index.html");
+}
+function page2(){
+    location.replace("projectComponents.html");
+}
+function page3(){
+    location.replace("distribution.html");
+}
+function page4(){
+    location.replace("team.html");
+}
